7b4c9b37d8a77eb1a97433425e5b6a747e533019
hangnah <hannah.pham@gmail.com>
2019-09-30 23:51:00 -0700

    added scrolling to body

--- vendor/jquery/projects.js
+++ vendor/jquery/projects.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    // fulcrum labs
     $('#design1 .tabCarousel a').click(function (event) {
         event.preventDefault();
         $('#design1 .tabCarousel a').removeClass('tabActive');
@@ -16,5 +16,17 @@ $(document).ready(function () {
         event.preventDefault();
         $('#design2 .tabCarousel a').removeClass('tabActive');
         $(this).addClass('tabActive');
+    });
+
+    // Add smooth scrolling to all links
+    $("#overview-nav a").on('click', function (event) {
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        $('html,body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500);
     });
 });
